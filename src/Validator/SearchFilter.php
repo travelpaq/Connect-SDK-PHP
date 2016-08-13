f30cef29eb73f14828fb17785504b865507c6d8a
Maximiliano Alves Pinheiro <maximilianoalvespinheiro@gmail.com>
2016-08-13 11:12:37 -0300

    descripcion de schemas y SearchFilter terminada

--- src/Validator/SearchFilter.php
+++ src/Validator/SearchFilter.php
@@ -41,11 +41,12 @@
 
      class SearchFilter {
    		var $data;
+   		var $schema = '{"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"order_field":{"type":"string","enum":["PRICE","DAPARTURE_DATE"],},"order_type":{"type":"string","enum":["ASC","DESC"],},"currency":{"type":"string","enum":["EUR","USD","ARS"],},"origin_place":{"type":"string","minLength":2,"maxLength":3,},"destination_place":{"type":"string","minLength":2,"maxLength":3,},"month_departure":{"type":"integer","minimum":1,"maximum":12,},"year_departure":{"type":"integer",},"Room":{"type":"array","minItems":1,"maxItems":4,"items":{"type":"object","properties":{"adult":{"type":"integer","minimum":1,"maximum":8,},"Children":{"type":"array","minItems":1,"maxItems":8,"items":{"type":"object","properties":{"age":{"type":"integer","minimum":0,"maximum":17,}},"required":["age"]}}},"required":["adult","Children"]}}},"required":["order_type","order_field","currency","origin_place","destination_place","month_departure","year_departure","Room"]}';
 		function _contruct($params){
 			$data = new Data($params)
 		}
 
 		public function validate(){
-			return Jsv4::validate(json_encode($this->data), '');
+			return Jsv4::validate(json_encode($this->data), $this->schema);
 		}
    }
