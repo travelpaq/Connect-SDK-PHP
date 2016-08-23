fc3e15c6e7e29d52cfe49bddc70af982bfcb9fde
Facundo Gonzalez <gonzalezfj@unsj-cuim.edu.ar>
2016-08-23 16:09:49 -0300

    ejemplo

--- ejemplo.php
+++ ejemplo.php
@@ -9,13 +9,13 @@ if(isset($_GET['page'])){
 {
 	$page = 0;
 }
-$a = '{"order_type":"ASC","order_field":"DAPARTURE_DATE","currency":"ARS","origin_place":"EZE","destination_place":"IGR","month_departure":9,"year_departure":2016,"Room":[{"adult":2,"Children":[]}]}';
+$a = '{"order_type":"ASC","order_field":"PRICE","currency":"USD","origin_place":"EZE","destination_place":"IGR","month_departure":9,"year_departure":2016,"Room":[{"adult":2,"Children":[]}]}';
 
 $b = json_decode($a,true);
-
 
 $tp = new PackagesAPI(['api_key' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjoiNTAifQ.35LFiBOikbD0zQxSlXKe6_t2jltNxQc3FZm5cMGKssA',
 					   'item_per_page' => 2]);
 echo "<pre>";
 $x = $tp->getPackageList($b,$page);
+//$x = $tp->getPackage($page);
 echo json_encode(json_decode($x),JSON_PRETTY_PRINT);
