3465f1945361da8a9e7801253c0ef3bf4e7e5dfb
Facundo Gonzalez <gonzalezfj@unsj-cuim.edu.ar>
2016-09-05 17:28:46 -0300

    eliminado DSSTORE

--- ejemplo.php
+++ ejemplo.php
@@ -1,5 +1,5 @@
 <?php
-
+echo phpinfo();
 require "vendor/autoload.php";
 
 use TravelPAQ\PackagesAPI\PackagesAPI;
@@ -16,6 +16,7 @@ $b = json_decode($a,true);
 $tp = new PackagesAPI(['api_key' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpIjoiNTAifQ.35LFiBOikbD0zQxSlXKe6_t2jltNxQc3FZm5cMGKssA',
 					   'item_per_page' => 2]);
 echo "<pre>";
-$x = $tp->getPackageList($b,$page);
-//$x = $tp->getPackage($page);
-echo json_encode(json_decode($x),JSON_PRETTY_PRINT);
+$list = $tp->getPackageList($b,$page);
+$package = $tp->getPackage($page);
+echo json_encode(json_decode($list),JSON_PRETTY_PRINT);
+var_dump($x);
