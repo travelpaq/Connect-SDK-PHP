e5703176fd5fed3894d7b57f0f0110c4a65c5988
Maximiliano Alves Pinheiro <maximilianoalvespinheiro@gmail.com>
2016-09-08 10:15:32 -0300

    nose

--- src/Exceptions/PackagesAPIException.php
+++ src/Exceptions/PackagesAPIException.php
@@ -11,31 +11,27 @@
  */
 namespace TravelPAQ\PackagesAPI\Exceptions;
 
-<<<<<<< HEAD
     abstract class PackagesAPIException extends \Exception implements IException
-=======
-abstract class PackagesAPIException extends \Exception implements IException
->>>>>>> bbcd316b68897d066cafdb68daa2383f40bc915d
-{
-    protected $message = 'Unknown exception';     // Exception message
-    private   $string;                            // Unknown
-    protected $code    = 0;                       // User-defined exception code
-    protected $file;                              // Source filename of exception
-    protected $line;                              // Source line of exception
-    private   $trace;                             // Unknown
+    {
+        protected $message = 'Unknown exception';     // Exception message
+        private   $string;                            // Unknown
+        protected $code    = 0;                       // User-defined exception code
+        protected $file;                              // Source filename of exception
+        protected $line;                              // Source line of exception
+        private   $trace;                             // Unknown
 
-    public function __construct($message = null, $code = 0)
-    {
-        if (!$message) {
-            throw new $this('Unknown '. get_class($this));
+        public function __construct($message = null, $code = 0)
+        {
+            if (!$message) {
+                throw new $this('Unknown '. get_class($this));
+            }
+            parent::__construct($message, $code, null);
         }
-        parent::__construct($message, $code, null);
+        
+        public function __toString()
+        {
+            return get_class($this) . " '{$this->message}' in {$this->file}({$this->line})\n"
+                                    . "{$this->getTraceAsString()}";
+        }
     }
-    
-    public function __toString()
-    {
-        return get_class($this) . " '{$this->message}' in {$this->file}({$this->line})\n"
-                                . "{$this->getTraceAsString()}";
-    }
-}
 ?>
