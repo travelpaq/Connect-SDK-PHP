2bd9a99e3913418d633333b830c7ca8cb6d07318
Facundo Gonzalez <gonzalezfj@unsj-cuim.edu.ar>
2016-09-12 18:32:43 -0300

    getpackage en forma  de objeto

--- src/Services/PackageService.php
+++ src/Services/PackageService.php
@@ -32,6 +32,10 @@ class PackageService extends Service
 						 ->request('GET',"Packages/getPackage/$id");
 		$body = $response->getBody()
 						 ->getContents();
-		return $body;
+		$body_decoded = json_decode($body,true);
+		if($body_decoded == null){
+			throw new Exception("Json mal formado");
+		}
+		return new Package($body_decoded);
 	}
 }
