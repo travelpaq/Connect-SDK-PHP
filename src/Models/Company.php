19d0dd0c024ae2017dc68f408976eb60a1f9ec68
Maximiliano Alves Pinheiro <maximilianoalvespinheiro@gmail.com>
2016-10-05 12:36:45 -0300

    Company.php model en sdk

---
+++ src/Models/Company.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * TravelPAQ Connect Api 
+ *
+ * @package  TravelPAQ
+ * 
+ * @author   Facundo J Gonzalez <facujgg@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace TravelPAQ\PackagesAPI\Models;
+
+use TravelPAQ\PackagesAPI\Models\Exceptions\ValidationException;
+/**
+ * Class Country
+ *
+ * @package TravelPAQ
+ */
+class Company
+{
+    public $name;
+    public $id;
+    public $cuil;
+    /**
+     * Constructor
+     * @param Array data datos de un Pais
+     */
+    public function __construct($data)
+    {
+    	if(!array_key_exists('name', $data))
+            $data['name'] = "";
+        $this->name = $data['name'];
+        
+        if(!array_key_exists('id', $data))
+            $data['id'] = "";
+        $this->iata = $data['id'];
+
+        if(!array_key_exists('cuil', $data))
+            $data['cuil'] = "";
+        $this->cuil = $data['cuil'];
+    }
+
+}
