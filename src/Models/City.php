2dc06b3e58efaed3697c62e1625982590308af2c
Facundo Gonzalez <gonzalezfj@unsj-cuim.edu.ar>
2016-10-14 16:21:22 -0300

    city->country para distinties

--- src/Models/City.php
+++ src/Models/City.php
@@ -13,33 +13,12 @@ namespace TravelPAQ\PackagesAPI\Models;
 
 use TravelPAQ\PackagesAPI\Models\Exceptions\ValidationException;
 /**
- * Class Place
+ * Class City
  *
  * @package TravelPAQ
  */
 class City extends Place implements \JsonSerializable
 {
-	public $name;
-	public $iata;
-    public $Country;
-    public $number_nights;
-    /**
-     * Constructor
-     * @param Array data datos de un lugar de un tramo de una ruta
-     */
-    public function __construct($data)
-    {
-        $this->Country = new Country($data['Country']);
-    	
-        if(!array_key_exists('name', $data))
-    		$data['name'] = "";
-    	$this->name = $data['name'];
-    	
-    	if(!array_key_exists('iata', $data))
-    		$data['iata'] = "";
-    	$this->iata = $data['iata'];
-    	
-    }
 
     public function jsonSerialize() {
         return [
