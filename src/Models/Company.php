5fb4e821f397b4a574921643bf50fd395dee1b81
Maximiliano Alves Pinheiro <maximilianoalvespinheiro@gmail.com>
2016-10-17 17:58:52 -0300

    Correccion  iata por id

--- src/Models/Company.php
+++ src/Models/Company.php
@@ -34,7 +34,7 @@ class Company
         
         if(!array_key_exists('id', $data))
             $data['id'] = "";
-        $this->iata = $data['id'];
+        $this->id = $data['id'];
 
         if(!array_key_exists('cuit', $data))
             $data['cuit'] = "";
