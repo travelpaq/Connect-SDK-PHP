075bfd7b03862ee4fd18be4153c72af2c73b3668
Unknown <maximilianoalvespinheiro@gmail.com>
2017-10-03 17:36:25 -0300

    Enriquecimiento de datos de hotel

--- src/Models/Hotel.php
+++ src/Models/Hotel.php
@@ -22,29 +22,91 @@ class Hotel
     public $star_rating;
     public $name;
     public $Place;
-    public $required_fields = [
-        "star_rating",
-        "name",
-        "Place"
-    ];
+    public $address;
+    public $lat;
+    public $lng;
+    public $url;
+    public $star_rating_trip_advisor;
+    public $popularity;
+    public $description;
+    public $Regimes;
+    public $RoomsKind;
+    public $Image;
+
     /**
      * Constructor
      * @param Array data datos de la salida
      */
     public function __construct($data)
     {
+        if(array_key_exists('Place', $data))
+            $this->Place = new Place($data['Place']);
+        else 
+            $this->Place = [];
+        
+        if(array_key_exists('name', $data))
+            $this->name = $data['name'];
+        else 
+            $this->name = '';
+        
+        if(array_key_exists('star_rating', $data))
+            $this->star_rating = (int)$data['star_rating'];
+        else 
+            $this->star_rating = 0;
+        
+        if(array_key_exists('address', $data))
+            $this->address = $data['address'];
+        else 
+            $this->address = '';
+        
+        if(array_key_exists('lat', $data))
+            $this->lat = $data['lat'];
+        else 
+            $this->lat = '';
+        
+        if(array_key_exists('lng', $data))
+            $this->lng = $data['lng'];
+        else 
+            $this->lng = '';
+        
+        if(array_key_exists('url', $data))
+            $this->url = $data['url'];
+        else 
+            $this->url = '';
+        
+        if(array_key_exists('star_rating_trip_advisor', $data))
+            $this->star_rating_trip_advisor = (int)$data['star_rating_trip_advisor'];
+        else 
+            $this->star_rating_trip_advisor = 0;
+        
+        if(array_key_exists('popularity', $data))
+            $this->popularity = (int)$data['popularity'];
+        else 
+            $this->popularity = 0;
+        
+        if(array_key_exists('description', $data))
+            $this->description = $data['description'];
+        else 
+            $this->description = '';
 
-        $hotelKeys = array_keys($data);
-        foreach ($this->required_fields as $required_field) {
-            if(!in_array($required_field,$hotelKeys))
-              throw new ValidationException("Falta el campo $required_field");
+        if(array_key_exists('Regimes', $data))
+            $this->Regimes = $data['Regimes'];
+        else 
+            $this->Regimes = [];
+
+        if(array_key_exists('RoomsKind', $data))
+            $this->RoomsKind = $data['RoomsKind'];
+        else 
+            $this->RoomsKind = [];
+
+
+        $this->Image = [];
+        if(array_key_exists('Image', $data)){
+            foreach ($data['Image'] as $key => $value) {
+                $this->Image[] = new Image($value);
+            }
         }
 
-        $this->Place = new Place($data['Place']);
-
-        $this->name = $data['name'];
-        
-        $this->star_rating = (int)$data['star_rating'];
     }
 
 }
